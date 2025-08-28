f5415f61499015d625640f0d84f7b050f574591e
ZlothY <badzacgaming@gmail.com>
2025-08-29 00:35:40 +0100

    Update PluginInfo.cs

--- dingus/PluginInfo.cs
+++ dingus/PluginInfo.cs
@@ -4,6 +4,6 @@
     {
         public const string GUID = "gizmo.zlothy.dingus";
         public const string Name = "dingus";
-        public const string Version = "1.1.0";
+        public const string Version = "1.1.1";
     }
 }
